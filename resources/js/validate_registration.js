de12562ea1d28f07d7b2297ae0ece4e0e22cec35
Connor\Desktop <connorgill@live.com.au>
2017-04-18 19:12:09 +1000

    Added registration validation

--- resources/js/validate_registration.js
+++ resources/js/validate_registration.js
@@ -1,29 +1,73 @@
 // Entrypoint function called once form is submitted
 function validate() {
-    var valid = true;
-    valid = (checkSomething(document.getElementsByClassName('')[0]) && valid)
-    return valid;
+    var valid = true
+    valid = (checkEmail(document.getElementsByClassName('email-field')[0]) && valid)
+    valid = (checkUsername(document.getElementsByClassName('username-field')[0]) && valid)
+    valid = (checkPassword(document.getElementsByClassName('password-field')[0]) && valid)
+    valid = (checkAge(document.getElementsByClassName('birth-picker')[0]) && valid)
+    valid = (checkGender(document.getElementsByClassName('male-radio')[0],
+        document.getElementsByClassName('female-radio')[0],
+        document.getElementsByClassName('other-radio')[0])
+        && valid)
+    valid = (checkTermsAndConditions(document.getElementsByClassName('agree-checkbox')[0]) && valid)
+    return false
 }
 
-// Check 'x' input
-function checkSomething(element) { // TODO
-    if (element.value == "") return showError('');
-    return true;
+// Check 'email-field' input
+function checkEmail(element) {
+    //if (element.value == "") return showError('')
+    return true
+}
+
+// Check 'username-field' input
+function checkUsername(element) {
+    if (element.value.length < 5) return showError('username-short-error')
+    if (element.value.length > 16) return showError('username-long-error')
+    return true
+}
+
+// Check 'password-field' input
+function checkPassword(element) {
+    if (element.value.length < 5) return showError('password-short-error')
+    if (element.value.length > 22) return showError('password-long-error')
+    if (element.value.replace(/[^0-9]/g,"").length < 2) return showError('password-number-error')
+    return true
+}
+
+// Check 'birth-picker' input
+function checkAge(element) {
+    var year = parseInt(element.value.split('-')[0])
+    if (element.value == "" || element.value == " " || element.value == null) return showError('age-error')
+    if (year >= 2010) return showError('age-young-error')
+    if (year < 1917) return showError('age-old-error')
+    return true
+}
+
+// Check 'male-radio', 'female-radio', 'other-radio' inputs
+function checkGender(element1, element2, element3) {
+    if (element1.checked == false && element2.checked == false && element3.checked == false) return showError('gender-error')
+    return true
+}
+
+// Check 'agree-checkbox' input
+function checkTermsAndConditions(element) {
+    if (element.checked == false) return showError('agree-error')
+    return true
 }
 
 // Function called to modify CSS properties to display a particular error
 function showError(errorName) {
-    document.getElementsByClassName('error-message')[0].style.display = 'block';
-    document.getElementsByClassName(errorName)[0].style.display = 'inline';
-    return false;
+    document.getElementsByClassName('error-message')[0].style.display = 'block'
+    document.getElementsByClassName(errorName)[0].style.display = 'inline'
+    return false
 }
 
 // Function resets all errors, called when any input is modified
 function resetError() {
     errorMessageContainer = document.getElementsByClassName('error-message')[0]
-    errorMessageContainer.style.display = 'none';
+    errorMessageContainer.style.display = 'none'
     for (var i = 0; i < errorMessageContainer.children.length; i++) {
-        var currentElement = errorMessageContainer.children[i];
-        currentElement.style.display = 'none';
+        var currentElement = errorMessageContainer.children[i]
+        currentElement.style.display = 'none'
     }
 }
--- resources/js/validate_search.js
+++ resources/js/validate_search.js
@@ -1,44 +1,44 @@
 // Entrypoint function called once form is submitted
 function validate() {
-    var valid = true;
+    var valid = true
     valid = (checkName(document.getElementsByClassName('name-field')[0]) && valid)
     valid = (checkSuburb(document.getElementsByClassName('suburb-picker')[0]) && valid)
     valid = (checkRating(document.getElementsByClassName('rating-picker')[0]) && valid)
-    return valid;
+    return valid
 }
 
 // Check 'name-field' input
 function checkName(element) {
-    if (element.value == "") return showError('name-error');
-    return true;
+    if (element.value == "") return showError('name-error')
+    return true
 }
 
 // Check 'suburb-picker' input
 function checkSuburb(element) {
-    if (element.value == "default") return showError('suburb-error');
-    return true;
+    if (element.value == "default") return showError('suburb-error')
+    return true
 }
 
 // Check 'rating-picker' input
 function checkRating(element) {
     if (element.value == null || element.value == "") return showError('rating-error')
     if (element.value > 5 || element.value < 1) return showError('rating-num-error')
-    return true;
+    return true
 }
 
 // Function called to modify CSS properties to display a particular error
 function showError(errorName) {
     document.getElementsByClassName('error-message')[0].style.display = 'block';
     document.getElementsByClassName(errorName)[0].style.display = 'inline';
-    return false;
+    return false
 }
 
 // Function resets all errors, called when any input is modified
 function resetError() {
     errorMessageContainer = document.getElementsByClassName('error-message')[0]
-    errorMessageContainer.style.display = 'none';
+    errorMessageContainer.style.display = 'none'
     for (var i = 0; i < errorMessageContainer.children.length; i++) {
-        var currentElement = errorMessageContainer.children[i];
-        currentElement.style.display = 'none';
+        var currentElement = errorMessageContainer.children[i]
+        currentElement.style.display = 'none'
     }
 }
